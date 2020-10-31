0f21aa2fc18a3b5a7c4103111ec6e56a2a4defa1
Michael Bell <Michael@home.den>
2020-10-31 20:42:12 +1100

    added reboot, shutdown, bug fixes

--- request.py
+++ request.py
@@ -1,8 +1,18 @@
 import pan_client
 
 def reboot(panclient: pan_client.PanClient):
-    response = panclient.get_xml_response("op", {"cmd" : "<request><restart><system></system></restart></request>"})
+    try:
+        response = panclient.get_xml_response("op", {"cmd" : "<request><restart><system></system></restart></request>"})
+    except Exception as e:
+        #All good - we rebooted here so it will time out
+        pass
+    
+
 
 def shutdown(panclient: pan_client.PanClient):
-    response = panclient.get_xml_response("op", {"cmd" : "<request><shutdown><system></system></shutdown></request>"})
+    try:
+        response = panclient.get_xml_response("op", {"cmd" : "<request><shutdown><system></system></shutdown></request>"})
+    except Exception as e:
+        #All good - we rebooted here so it will time out
+        pass
 
